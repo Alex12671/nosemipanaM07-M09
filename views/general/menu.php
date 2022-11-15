cd0b9d1050c9e411d3429c645a248a29f47cb22c
Alex12671 <113857626+Alex12671@users.noreply.github.com>
2022-11-15 12:20:48 +0100

    Add files via upload

--- views/general/menu.php
+++ views/general/menu.php
@@ -1,9 +1,42 @@
-<ul>
-    <li> <a href= "index.php?controller=Usuario&action=crear" >Nuevo usuario </a></li>
-    <li> <a href= "index.php?controller=Usuario&action=modificar" >Modificar usuario </a></li>
-    <li> <a href= "index.php?controller=Usuario&action=eliminar" >Eliminar usuario </a></li>
-    <li> <a href= "index.php?controller=Usuario&action=mostrarTodos" >Mostrar todos los usuarios </a></li>
-    <li> <a href= "index.php?controller=admin&action=logAdmin" >login admin </a></li>
-    <li> <a href= "views/general/sortir.php" >salir </a></li>
+<nav class="navegador">
+    <ul>
+        <li> <a href= 'index.php?controller=admin&action=logAdmin' >login admin </a></li>
+        <li> <a href= 'index.php?controller=user&action=showRegisterForm' >Registrarse</a></li>
+        <li class="bookCategory">Libros
+            <ul class="filterCategories" >
+                <?php
+                    while ($array = $show->fetchAll(PDO::FETCH_ASSOC)) {
+                       foreach ($array as $genero => $value) {
+                            echo "<li><a href='index.php?controller=product&action=SearchProductsByCategory&id=".$value['IdGenero']."'>".$value['Nombre']."</a></li>";
+                       }
+                    }
+                ?>
+            </ul>
+        </li>
+    <?php
+        if(isset($_SESSION['rol']) && $_SESSION['rol'] == 'comprador') {
+            ?> 
+            <li>Mi cuenta UwU
+                <ul class="filterCategories" >
+                    <?php
+                        echo "<li> <a href= 'index.php?controller=user&action=logUser' >Datos de mi cuenta</a></li>"; 
+                        echo "<li> <a href= 'index.php?controller=user&action=logUser' >Ver mis pedidos</a></li>"; 
+                    ?>
+                </ul>
+            </li>
+        <?php
+        }
+        else {
+            echo "<li> <a href= 'index.php?controller=user&action=logUser' >Iniciar sesión</a></li>";
 
-</ul>
+        }
+    ?>
+    <li><?php echo "<form action= 'index.php?controller=product&action=SearchProductsByName' method='post'>"; ?>
+            <input type='text' name='bookFilter'/>
+            <input type='submit' value='Buscar'/>
+        </form>
+    </li>
+    </ul>
+</nav>
+
+
