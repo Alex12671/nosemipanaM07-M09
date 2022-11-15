0c361bd5500ba4cbdd52a483f4d143b903de00fe
Alex12671 <113857626+Alex12671@users.noreply.github.com>
2022-11-15 12:21:57 +0100

    Add files via upload

--- views/general/menu.php
+++ views/general/menu.php
@@ -13,6 +13,29 @@
                 ?>
             </ul>
         </li>
+    <?php
+        if(isset($_SESSION['rol']) && $_SESSION['rol'] == 'comprador') {
+            ?> 
+            <li>Mi cuenta UwU
+                <ul class="filterCategories" >
+                    <?php
+                        echo "<li> <a href= 'index.php?controller=user&action=logUser' >Datos de mi cuenta</a></li>"; 
+                        echo "<li> <a href= 'index.php?controller=user&action=logUser' >Ver mis pedidos</a></li>"; 
+                    ?>
+                </ul>
+            </li>
+        <?php
+        }
+        else {
+            echo "<li> <a href= 'index.php?controller=user&action=logUser' >Iniciar sesión</a></li>";
+
+        }
+    ?>
+    <li><?php echo "<form action= 'index.php?controller=product&action=SearchProductsByName' method='post'>"; ?>
+            <input type='text' name='bookFilter'/>
+            <input type='submit' value='Buscar'/>
+        </form>
+    </li>
     </ul>
 </nav>
 
