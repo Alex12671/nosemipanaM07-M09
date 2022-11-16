bb789927e31f8fdd59f3cccb8229c9744d24767f
Alex12671 <113857626+Alex12671@users.noreply.github.com>
2022-11-16 10:55:13 +0100

    Add files via upload

--- views/products/productFilter.php
+++ views/products/productFilter.php
@@ -1,7 +1,9 @@
 <?php 
     while ($array = $filter->fetchAll(PDO::FETCH_ASSOC)) {
         foreach ($array as $genero => $value) {
-            echo "".$value['IdProducto']."".$value['Nombre']."";
+            echo "<img class='productImage' src=".$value['Imagenlibro']."></img>";
+            echo "<h2>".$value['Nombre']."</h2>";
+            echo "<h2>".$value['Precio']."</h2>"; 
         }
      }
 ?>
