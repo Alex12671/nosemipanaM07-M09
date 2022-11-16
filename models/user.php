9c61984271ccd1181c9794b399282953d1d1814c
PauRodriguezGuzman412 <113432167+PauRodriguezGuzman412@users.noreply.github.com>
2022-11-16 09:48:12 +0100

    Add files via upload

--- models/user.php
+++ models/user.php
@@ -301,9 +301,13 @@ class User extends Database {
     }
 
     public function ShowUserOrders($id){
-        $sql = "SELECT * FROM pedidos where IdCliente='$id'";
+        $sql = "SELECT producto.Nombre, producto.Precio, pedidos.Precio_sin_IVA, pedidos.Precio_IVA, linea_pedido.Cantidad, pedidos.Estado_Pedido
+        FROM linea_pedido INNER JOIN pedidos ON linea_pedido.IdPedido = pedidos.IdPedido 
+        INNER JOIN producto ON linea_pedido.IdProducto = producto.IdProducto WHERE IdCliente=$id
+        ORDER BY linea_pedido.Id_Linea_Pedido DESC";
         $result = $this->db->query($sql);
-        return $result;
+        $array = $result->fetchAll(PDO::FETCH_ASSOC);
+        return $array;
     }
 }
 ?>
