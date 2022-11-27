69a675c48871ecd31c73f1ee30bd470aeef4ec87
raulalcaldecabo <113103832+raulalcaldecabo@users.noreply.github.com>
2022-11-27 17:45:43 +0100

    Add files via upload

--- views/products/productDetails.php
+++ views/products/productDetails.php
@@ -1,54 +1,62 @@
-<?php
-while ($array = $details->fetch(PDO::FETCH_ASSOC)) {
-    echo "<img src=".$array['Imagenlibro']."></img>";
-    echo "<h1>".$array['Nombre']."</h1>";
-    echo "<h2>".$array['Precio']."€</h2>"; 
-    echo "<h3>".$array['Descripcion']."</h3>"; 
-    echo "<h2><a href='index.php?controller=product&action=AddProductToCart' >Añadir al carro</a></h2>";
-    if(isset($_COOKIE['aceptado'])) {
-        if($_COOKIE['aceptado'] == 1) {
-            if(!isset($_COOKIE['lastVisitedBooks'])) {
-                $lastVisited = array
-                (
-                    0 => $array['IdProducto'],
-                );
-                $json = json_encode($lastVisited);
-                setcookie('lastVisitedBooks', $json,time() + (60 * 60 * 24 * 365));
-            }
-            else {
-                $length = count(json_decode($_COOKIE['lastVisitedBooks'],true));
-                if($length == 3){
-                   $lastVisited = json_decode($_COOKIE['lastVisitedBooks'],true);
-                    if(!in_array($array['IdProducto'],$lastVisited)) {
-                        array_shift($lastVisited);
-                        array_push($lastVisited,$array['IdProducto']);
+<div class="container">
+    <div class="detail">
+        <?php
+        while ($array = $details->fetch(PDO::FETCH_ASSOC)) {
+            echo "<img class='imgDetail' src=".$array['Imagenlibro']."></img>";
+            echo "<h2 class='encabezado titledetail'>".$array['Nombre']."</h2>";
+            echo "<div class='descripcion'>";
+            echo "<h3>".$array['Descripcion']."</h3>";
+            echo "</div>";
+            echo "<div class='precio'>";
+            echo "<h2>".$array['Precio']."€</h2>";
+            echo "<h2><a href='index.php?controller=product&action=AddProductToCart' >Añadir al carro</a></h2>";
+            echo "</div>";
+            if(isset($_COOKIE['aceptado'])) {
+                if($_COOKIE['aceptado'] == 1) {
+                    if(!isset($_COOKIE['lastVisitedBooks'])) {
+                        $lastVisited = array
+                        (
+                            0 => $array['IdProducto'],
+                        );
+                        $json = json_encode($lastVisited);
+                        setcookie('lastVisitedBooks', $json,time() + (60 * 60 * 24 * 365));
                     }
                     else {
-                        $clave = array_search($array['IdProducto'],$lastVisited);
-                        array_splice($lastVisited,$clave,1);
-                        array_push($lastVisited,$array['IdProducto']);
+                        $length = count(json_decode($_COOKIE['lastVisitedBooks'],true));
+                        if($length == 3){
+                           $lastVisited = json_decode($_COOKIE['lastVisitedBooks'],true);
+                            if(!in_array($array['IdProducto'],$lastVisited)) {
+                                array_shift($lastVisited);
+                                array_push($lastVisited,$array['IdProducto']);
+                            }
+                            else {
+                                $clave = array_search($array['IdProducto'],$lastVisited);
+                                array_splice($lastVisited,$clave,1);
+                                array_push($lastVisited,$array['IdProducto']);
+                            }
+                            $json = json_encode($lastVisited);
+                            setcookie('lastVisitedBooks', $json,time() + (60 * 60 * 24 * 365));
+                        }
+                        else if($length > 1) {
+                            $lastVisited = json_decode($_COOKIE['lastVisitedBooks'],true);
+                            if(!in_array($array['IdProducto'],$lastVisited)) {
+                                array_push($lastVisited,$array['IdProducto']);
+                            }
+                            $json = json_encode($lastVisited);
+                            setcookie('lastVisitedBooks', $json,time() + (60 * 60 * 24 * 365));
+                        }
+                        else {
+                            $lastVisited = json_decode($_COOKIE['lastVisitedBooks'],true);
+                            if(!in_array($array['IdProducto'],$lastVisited)) {
+                                array_push($lastVisited,$array['IdProducto']);
+                            }
+                            $json = json_encode($lastVisited);
+                            setcookie('lastVisitedBooks', $json,time() + (60 * 60 * 24 * 365));
+                        }
                     }
-                    $json = json_encode($lastVisited);
-                    setcookie('lastVisitedBooks', $json,time() + (60 * 60 * 24 * 365));
-                }
-                else if($length > 1) {
-                    $lastVisited = json_decode($_COOKIE['lastVisitedBooks'],true);
-                    if(!in_array($array['IdProducto'],$lastVisited)) {
-                        array_push($lastVisited,$array['IdProducto']);
-                    }
-                    $json = json_encode($lastVisited);
-                    setcookie('lastVisitedBooks', $json,time() + (60 * 60 * 24 * 365));
-                }
-                else {
-                    $lastVisited = json_decode($_COOKIE['lastVisitedBooks'],true);
-                    if(!in_array($array['IdProducto'],$lastVisited)) {
-                        array_push($lastVisited,$array['IdProducto']);
-                    }
-                    $json = json_encode($lastVisited);
-                    setcookie('lastVisitedBooks', $json,time() + (60 * 60 * 24 * 365));
                 }
             }
         }
-    }
-}
-?>
+        ?>
+    </div>
+</div>
