5e61ae109e1010c6fa436a31b7a5dcd89542994f
Alex12671 <113857626+Alex12671@users.noreply.github.com>
2022-11-30 10:53:13 +0100

    Add files via upload

--- views/general/menu.php
+++ views/general/menu.php
@@ -14,31 +14,31 @@
                 ?>
             </ul>
         </li>
-        <?php
-            if(isset($_SESSION['rol']) && $_SESSION['rol'] == 'comprador') {
-        ?> 
+    <?php
+        if(isset($_SESSION['rol']) && $_SESSION['rol'] == 'comprador') {
+            ?> 
             <li>Mi cuenta UwU
-                    <ul class="filterCategories" >
-                        <?php
-                            echo "<li> <a href= 'index.php?controller=user&action=ShowUserProfile' >Datos de mi cuenta</a></li>"; 
-                            echo "<li> <a href= 'index.php?controller=user&action=ShowUserOrders' >Ver mis pedidos</a></li>";
-                            echo "<li> <a href= 'views/general/sortir.php' >Cerrar sesión</a></li>";  
-                        ?>
-                    </ul>
-                </li>
-            <?php
-            }
-            else {
-                echo "<li> <a href= 'index.php?controller=user&action=logUser' >Iniciar sesión</a></li>";
+                <ul class="filterCategories" >
+                    <?php
+                        echo "<li> <a href= 'index.php?controller=user&action=ShowUserProfile' >Datos de mi cuenta</a></li>"; 
+                        echo "<li> <a href= 'index.php?controller=user&action=ShowUserOrders' >Ver mis pedidos</a></li>";
+                        echo "<li> <a href= 'views/general/sortir.php' >Cerrar sesión</a></li>";  
+                    ?>
+                </ul>
+            </li>
+        <?php
+        }
+        else {
+            echo "<li> <a href= 'index.php?controller=user&action=logUser' >Iniciar sesión</a></li>";
 
-            }
-        ?>
-        <li><?php echo "<form action= 'index.php?controller=product&action=SearchProductsByName' method='post'>"; ?>
-                <input type='text' name='bookFilter'/>
-                <input type='submit' value='Buscar'/>
-            </form>
-        </li>
-        <li class="cartMenu" > <a href="uwu.php" > <img class="cartIcon" src="img/cart.png" > </a> </li>
+        }
+    ?>
+    <li><?php echo "<form action= 'index.php?controller=product&action=SearchProductsByName' method='post'>"; ?>
+            <input type='text' name='bookFilter'/>
+            <input type='submit' value='Buscar'/>
+        </form>
+    </li>
+    <li class="cartMenu" > <a href="index.php?controller=product&action=ShowCart" > <img class="cartIcon" src="img/cart.png" > </a> </li>
     </ul>
 </nav>
 
