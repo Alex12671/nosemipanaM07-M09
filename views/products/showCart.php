249d7f472264c5db1a2f13a7c699c1d499653b53
Alex12671 <113857626+Alex12671@users.noreply.github.com>
2023-01-11 09:18:49 +0100

    Add files via upload

--- views/products/showCart.php
+++ views/products/showCart.php
@@ -7,9 +7,11 @@
     if(isset($_SESSION['Cart']) || !empty($_SESSION['Cart'])) {
         if(isset($_POST['quantity'])) {
             $precioUnitario = $_SESSION['Cart'][$_GET['id']]['Price'] / $_SESSION['Cart'][$_GET['id']]['Quantity'];
-            $precioOriginalUnitario = $_SESSION['Cart'][$_GET['id']]['OriginalPrice'] / $_SESSION['Cart'][$_GET['id']]['Quantity'];
+            if(isset($_SESSION['Cart'][$_GET['id']]['OriginalPrice'])) {
+                $precioOriginalUnitario = $_SESSION['Cart'][$_GET['id']]['OriginalPrice'] / $_SESSION['Cart'][$_GET['id']]['Quantity'];
+                $_SESSION['Cart'][$_GET['id']]['OriginalPrice'] = $precioOriginalUnitario * $_POST['quantity'];
+            }
             $_SESSION['Cart'][$_GET['id']]['Price'] = $precioUnitario * $_POST['quantity'];
-            $_SESSION['Cart'][$_GET['id']]['OriginalPrice'] = $precioOriginalUnitario * $_POST['quantity'];
             $_SESSION['Cart'][$_GET['id']]['Quantity'] = $_POST['quantity'];
             $_SESSION['TotalQuantity'] = 0;
         foreach($_SESSION['Cart'] as $data) {
@@ -26,11 +28,10 @@
                     echo "<p class='bookTitle'>".$value['Nombre']."</p>";
                     echo "<p class='bookAuthor'>".$value['Autor']."</p>";
                     echo "<form method='POST' action='index.php?controller=product&action=ShowMain&id=".$data."' id='quantityForm'>";
-                    echo "<input type='number' name='quantity' id='quantity' min='1' value=".$_SESSION['Cart'][$data]['Quantity']." onchange='checkQuantityInput()'></p>";
+                    echo "<input type='number' name='quantity' id='quantity' min='1' max='".$_SESSION['Cart'][$data]['Stock']."' value=".$_SESSION['Cart'][$data]['Quantity']." onchange='checkQuantityInput()'></p>";
                     echo "</form>";
                     if($_SESSION['Cart'][$data]['Liquidacion']==0){
-                        $_SESSION['Cart'][$data]['Price'] = number_format($_SESSION['Cart'][$data]['Price'],2);
-                        echo "<p class='linePrice' >".$_SESSION['Cart'][$data]['Price']."€</p>";
+                        echo "<p class='linePrice' >".number_format( (float) $_SESSION['Cart'][$data]['Price'],2)."€</p>";
                     }
                     else if($_SESSION['Cart'][$data]['Liquidacion']==1){
                         $_SESSION['Cart'][$data]['Price']=$_SESSION['Cart'][$data]['Price'];
@@ -53,17 +54,17 @@
         foreach($_SESSION['Cart'] as $data) {
             $total += $data['Price'];
         } 
-        echo "<p>Subtotal: ".$total."€</p>";
+        echo "<p>Subtotal: ".number_format((float) $total,2)."€</p>";
         if($total > 30) {
             echo "<p>Gastos de envío: Gratis </p>";
             echo "<hr class='summarySeparator'>";
-            echo "<p>TOTAL: <span class='totalPrice' >".$total."€ </span> </p>";
+            echo "<p>TOTAL: <span class='totalPrice' >".number_format((float) $total,2)."€ </span> </p>";
         }
         else {
             $total += 4.99;
             echo "<p>Gastos de envío: 4.99€ </p>";
             echo "<hr class='summarySeparator'>";
-            echo "<p>TOTAL: <span class='totalPrice' >".$total."€ </span> </p>";
+            echo "<p>TOTAL: <span class='totalPrice' >".number_format((float) $total,2)."€ </span> </p>";
         }
         ?>
         <form action="index.php?controller=order&action=AddOrder&total=<?php echo $total; ?>" method="POST">
